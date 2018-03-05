e41634cb4ba11596297ebb718e1961f29045aeba
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-03-05 16:27:13 +0800

    add script to create github repos

--- misc/i3status.sh
+++ misc/i3status.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-i3status -c ~/.i3/i3status.conf | while :
-do
-  read line
-  pomodoro=`python ~/repos/i3-gnome-pomodoro/pomodoro-client.py status`
-  echo "$pomodoro| $line" || exit 1
-done
+#i3status -c ~/.config/i3/i3blocks.conf | while :
+#do
+#  read line
+#  pomodoro=`python ~/Apps/i3-gnome-pomodoro/pomodoro-client.py status`
+#  echo "$pomodoro| $line" || exit 1
+#done
+
+pomodoro=`python ~/Apps/i3-gnome-pomodoro/pomodoro-client.py status`
+echo "$pomodoro"
