2ba4029eda22c39e3e416907b6ce98380a8abb07
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-11-19 13:58:41 +0800

    modify backup scripts

--- backup/pictures_bak.sh
+++ backup/pictures_bak.sh
@@ -2,23 +2,23 @@
 
 #SomeDirectory backup script
 #Today's date in ISO-8601 format:
-DATE=`date +"\%Y-\%b-\%d_\%T"`
+DATE=`date +"%Y-%b-%d_%T"`
 #DATE=`date -I`
 #The source directory:
-SRC="/home/pawel/"
+SRC="/home/pawel/Pictures/"
 #Include the following files:
 #INCL="/home/pawel/scripts/backup/home_include"
 #Exclude the following files:
 EXCL="/home/pawel/Dropbox/Scripts/backup/home_ignorelist"
 #The target directory:
-TRG="/home/pawel/Desktop/usb/backup/home/"
+TRG="/home/pawel/Desktop/usb/backup/Pictures/"
 #The symlink directory of the last backup
 LAST=$TRG"last"
 #The location of log output file
 LOG=$TRG"backup.log"
 #The rsync options:
 #--exclude-from=$EXCL \
-OPT="-avh --list-only --info=progress2 --delete --include 'Documents/***' --link-dest=$LAST --log-file=$LOG"
+OPT="-avh --info=progress2 --delete --link-dest=$LAST --log-file=$LOG"
 
 #Check if directory is present
 if [ ! -d $TRG ]; then
